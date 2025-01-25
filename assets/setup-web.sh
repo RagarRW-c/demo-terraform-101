ab3862a27f683394f3b6672cf45f463e727feaf6
Witalij Rapicki <witalij.rapicki@gmail.com>
2025-01-25 20:17:39 +0100

    j

--- assets/setup-web.sh
+++ assets/setup-web.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
-cp /tmp/demo-terraform-101/demo-terraform-101/assets/webapp /usr/local/bin/
+cp /tmp/demo-terraform-101/assets/webapp /usr/local/bin/
 chmod +x /usr/local/bin/*
-cp /tmp/demo-terraform-101/demo-terraform-101/assets/webapp.service /lib/systemd/system/webapp.service
+cp /tmp/demo-terraform-101/assets/webapp.service /lib/systemd/system/webapp.service
 service webapp start
 systemctl enable webapp
 
